166a7edde7c1cdd41746a512906701ef4972f394
Mauricio <mauriciomolina01@gmail.com>
2022-01-31 23:04:07 +0100

    current user synced with redux

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
@@ -8,19 +9,15 @@ import Header from './components/header/header.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
+import { setCurrentUser } from './redux/user/user.actions';
+
 class App extends React.Component {
-
-	constructor() {
-		super();
-
-		this.state = {
-			currentUser: null
-		};
-	}
 
 	unsubscribeFromAuth = null
 
 	componentDidMount() {
+
+		const {setCurrentUser} = this.props;
 		
 		this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
 
@@ -28,15 +25,15 @@ class App extends React.Component {
 				const userRef = await createUserProfileDocument(userAuth);
 
 				userRef.onSnapshot(snapshot => {
-					this.setState({currentUser: {
+					setCurrentUser({
 							id: snapshot.id,
 							...snapshot.data()
 						}
-					});
+					);
 				});
 
 			} else {
-				this.setState({ currentUser: userAuth });
+				setCurrentUser(userAuth);
 			}
 			
 		});
@@ -49,7 +46,7 @@ class App extends React.Component {
 	render() {
 		return (
 			<div >
-				<Header currentUser={this.state.currentUser}/>
+				<Header />
 				<Routes>
 					<Route path='/' element={<HomePage />} />
 					<Route path='/shop' element={<ShopPage />} />
@@ -60,4 +57,8 @@ class App extends React.Component {
 	}
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+	setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(null, mapDispatchToProps)(App);

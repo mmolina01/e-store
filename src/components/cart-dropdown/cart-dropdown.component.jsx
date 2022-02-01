c5e9b83ed81d1f355eee7dd13e83a61b6bda43ef
Mauricio <mauriciomolina01@gmail.com>
2022-02-01 13:53:29 +0100

    cart icon/dropdown + cart reducer

---
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import CustomButtom from '../custom-button/custom-button.component'
+
+import './cart-dropdown.styles.scss';
+
+const CartDropdown = () => (
+	<div className="cart-dropdown">
+		<div className="cart-items"></div>
+
+		<CustomButtom>
+			GO TO CHECKOUT
+		</CustomButtom>
+	</div>
+);
+
+export default CartDropdown;

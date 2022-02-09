2919d626d78c206fea5119deec42a4277ceb4b78
Mauricio <mauriciomolina01@gmail.com>
2022-02-09 16:26:11 +0100

    fixed shop selector for collections

--- src/components/collections-overview/collectios-overview.component.jsx
+++ src/components/collections-overview/collectios-overview.component.jsx
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 
 import { createStructuredSelector } from "reselect";
 
-import { selectShopCollections } from "../../redux/shop/shop.selectors";
+import { selectShopCollectionsForPreview } from "../../redux/shop/shop.selectors";
 
 import CollectionPreview from "../collection-preview/collection-preview.component";
 
@@ -23,7 +23,7 @@ const CollectionOverview = ({collections}) => {
 };
 
 const mapStateToProps = createStructuredSelector({
-	collections: selectShopCollections
+	collections: selectShopCollectionsForPreview
 });
 
 export default connect(mapStateToProps)(CollectionOverview);

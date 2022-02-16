5abd3231a8a1c22e260ab348a51f4f56fda9f1ce
Mauricio <mauriciomolina01@gmail.com>
2022-02-16 19:55:10 +0100

    hooks in shop.component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
-import { connect } from "react-redux";
+import { useDispatch } from "react-redux";
 
 import { fetchCollectionsStart } from "../../redux/shop/shop.actions";
 
@@ -8,11 +8,13 @@ import CollectionsOverviewContainer from "../../components/collections-overview/
 import CollectionPageContainer from "../collection/collection.container";
 
 
-const ShopPage = ({fetchCollectionsStart } ) => {
+const ShopPage = () => {
+
+	const dispatch = useDispatch();
 
 	useEffect(() => {
-		fetchCollectionsStart();
-	}, [fetchCollectionsStart]);
+		dispatch(fetchCollectionsStart());
+	}, [dispatch]);
 
 	return (
 		<Routes>
@@ -22,8 +24,4 @@ const ShopPage = ({fetchCollectionsStart } ) => {
 	)
 }
 
-const mapDispatchToProps = dispatch =>({
-	fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
-});
-
-export default connect(null, mapDispatchToProps)(ShopPage);
+export default ShopPage;
